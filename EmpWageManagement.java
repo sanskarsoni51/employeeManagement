5d2936618523c259ec2d1d0b230e8745f83cc597
Sanskar soni <sanskarsoni.cse23@jecrc.ac.in>
2023-11-15 17:38:56 +0530

    UC4SwitchCase

--- EmpWageManagement.java
+++ EmpWageManagement.java
@@ -7,18 +7,21 @@ public class EmpWageManagement {
         final int WAGE_PER_HR = 20;
         int empType = (int) (Math.random() * 100) % 3;
         int workingHours = 0;
-        if (empType == FULL_TIME)
-        {
-            System.out.println("Employee is Present");
-            workingHours = 8;
-        }else if(empType == PART_TIME){
-            System.out.println("employee is present part time");
-            workingHours = 4;
-        } 
-        else
-        {
-            System.out.println("Employee is Absent");
+        switch (empType) {
+            case FULL_TIME:
+                System.out.println("Employee is Present");
+                workingHours = 8;
+                break;
+            case PART_TIME:
+                System.out.println("employee is present part time");
+                workingHours = 4;
+                break;
+        
+            default:
+                System.out.println("Employee is Absent");
+                break;
         }
+        
         int wage = workingHours * WAGE_PER_HR;
         System.out.println("Employee Daily Wage is " + wage);
     }
