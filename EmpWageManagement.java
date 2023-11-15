7122ef469036c03f601c5893b0d30072a3a656f9
Sanskar soni <sanskarsoni.cse23@jecrc.ac.in>
2023-11-15 17:30:48 +0530

    UC2CalculateWage

--- EmpWageManagement.java
+++ EmpWageManagement.java
@@ -2,6 +2,19 @@ package Problems.EmpoyeeManagementSystem;
 
 public class EmpWageManagement {
     public static void main(String[] Args){
-        System.out.println("welcome to Employee Wage computation");
+        final int FULL_TIME = 1;
+        final int WAGE_PER_HR = 20;
+        int empType = (int) (Math.random() * 100) % 2;
+        int workingHours = 0;
+        if (empType == FULL_TIME)
+        {
+            System.out.println("Employee is Present");
+            workingHours = 8;
+        } else
+        {
+            System.out.println("Employee is Absent");
+        }
+        int wage = workingHours * WAGE_PER_HR;
+        System.out.println("Employee Daily Wage is " + wage);
     }
 }
