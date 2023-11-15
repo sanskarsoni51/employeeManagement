9d868f8b8f34f867c1ca6d4ac11eeac201dde3fe
Sanskar soni <sanskarsoni.cse23@jecrc.ac.in>
2023-11-15 17:03:19 +0530

    initial hi he

--- EmpWageManagement.java
+++ EmpWageManagement.java
@@ -1,5 +1,7 @@
 package Problems.EmpoyeeManagementSystem;
 
 public class EmpWageManagement {
-    
+    public static void main(String[] Args){
+        System.out.println("welcome to Employee Wage computation");
+    }
 }
