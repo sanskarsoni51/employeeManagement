3245ddc8dbe204bcb63f45dfec1bd8b140739225
Sanskar soni <sanskarsoni.cse23@jecrc.ac.in>
2023-11-15 17:10:22 +0530

    Attendance check

--- EmpWageManagement.java
+++ EmpWageManagement.java
@@ -1,5 +1,14 @@
 package Problems.EmpoyeeManagementSystem;
 
 public class EmpWageManagement {
+    public static void main(String[] args) {
+        int IS_FULL_TIME = 1;
+        double check = Math.floor(Math.random()*10) % 2;
+        if(IS_FULL_TIME == check){
+            System.out.println("Employee is present");
+        }else{
+            System.out.println("employee is absent");
+        }
     
+}
 }
