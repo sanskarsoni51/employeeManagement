3e078c21837125186cbd03e9c4d5d8884b0a007a
Sanskar soni <sanskarsoni.cse23@jecrc.ac.in>
2023-11-15 17:49:39 +0530

    UC5MonthlyWages

--- EmpWageManagement.java
+++ EmpWageManagement.java
@@ -5,24 +5,22 @@ public class EmpWageManagement {
         final int PART_TIME = 1;
         final int FULL_TIME = 2;
         final int WAGE_PER_HR = 20;
-        int empType = (int) (Math.random() * 100) % 3;
-        int workingHours = 0;
-        switch (empType) {
-            case FULL_TIME:
-                System.out.println("Employee is Present");
-                workingHours = 8;
-                break;
-            case PART_TIME:
-                System.out.println("employee is present part time");
-                workingHours = 4;
-                break;
-        
-            default:
-                System.out.println("Employee is Absent");
-                break;
+        final int Working_Days = 20;
+
+        int totalWage = 0;
+        for(int i = 1;i<Working_Days;i++){
+            int empType = (int) (Math.random() * 100) % 3;
+            int workingHours = 0;
+            switch (empType) {
+                case FULL_TIME: workingHours = 8;
+                case PART_TIME: workingHours = 4;
+            
+                default: workingHours = 0;
+            }
+            int wage = workingHours * WAGE_PER_HR;
+            totalWage = totalWage + wage;
         }
+        System.out.println("total wage of the month is " + totalWage);
         
-        int wage = workingHours * WAGE_PER_HR;
-        System.out.println("Employee Daily Wage is " + wage);
     }
 }
